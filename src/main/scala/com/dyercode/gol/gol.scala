b436fafa91d06d23b0a3937b95299f7908af83d2
Jon <dyercode@users.noreply.github.com>
2021-06-23 13:39:38 -0400

    make alive or dead clearer w/ enum.
    drive in the start of a generation iteration.

--- src/main/scala/com/dyercode/gol/gol.scala
+++ src/main/scala/com/dyercode/gol/gol.scala
@@ -1,10 +1,57 @@
 package com.dyercode.gol
 
-type Board = (Int, Int) => Boolean
+enum Cell:
+  case Alive, Dead
 
-def getEmptyBoard: Board = (_: Int, _: Int) => false
+import Cell._
+type Board = (Int, Int) => Cell
+
+def emptyBoard: Board = (_: Int, _: Int) => Dead
 
 def addNewCell(board: Board, x: Int, y: Int): Board = {
   (newX: Int, newY: Int) =>
-    (newX == x && newY == y) || board(newX, newY)
+    if (newX == x && newY == y)
+      Alive
+    else
+      board(newX, newY)
+}
+
+def neighbors(x: Int, y: Int): Seq[(Int, Int)] = {
+  Seq(
+    (x + 1, y),
+    (x + 1, y + 1),
+    (x + 1, y - 1),
+    (x, y + 1),
+    (x, y - 1),
+    (x - 1, y),
+    (x - 1, y + 1),
+    (x - 1, y - 1)
+  )
+}
+
+case class Size(width: Int, height: Int)
+def evolve(board: Board, size: Size): Board = {
+  val nn: Seq[((Int, Int), Seq[(Int, Int)])] = for {
+    x <- 0 to size.width
+    y <- 0 to size.height
+  } yield ((x, y), neighbors(x, y))
+
+  nn.foldLeft(emptyBoard) {
+    (
+        acc: Board,
+        value: ((Int, Int), Seq[(Int, Int)])
+    ) =>
+      {
+        val (coords, neighbors) = value
+        val liveNeighborCount = neighbors.count { case (x, y) =>
+          board(x, y) == Alive
+        }
+        if (liveNeighborCount < 2) {
+          acc
+        } else {
+          acc
+        }
+      }
+  }
+
 }
--- src/test/scala/com/dyercode/gol/GameSuite.scala
+++ src/test/scala/com/dyercode/gol/GameSuite.scala
@@ -1,26 +1,36 @@
 package com.dyercode.gol
 
+import com.dyercode.gol.Cell.{Alive, Dead}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.must
 
 class GameSuite extends AnyFunSuite with must.Matchers {
 
   test("addNewCell must add a cell to the board at the given coordinates") {
-    val board = addNewCell(getEmptyBoard, 1, 1)
-    board(1, 1) mustBe true
+    val board = addNewCell(emptyBoard, 1, 1)
+    board(1, 1) mustBe Alive
   }
 
   test("addNewCell must maintain previous cells in the board") {
-    val board = addNewCell(getEmptyBoard, 1, 1)
+    val board = addNewCell(emptyBoard, 1, 1)
     val newBoard = addNewCell(board, 3, 3)
-    newBoard(1, 1) mustBe true
-    newBoard(3, 3) mustBe true
+    newBoard(1, 1) mustBe Alive
+    newBoard(3, 3) mustBe Alive
   }
 
+  // Todo - property test this instead of hardcoded wrong cells
   test("addNewCell must not have cells that weren't added") {
-    val board = addNewCell(getEmptyBoard, 1, 1)
+    val board = addNewCell(emptyBoard, 1, 1)
     val newBoard = addNewCell(board, 3, 3)
-    newBoard(4, 4) mustBe false
-    newBoard(7, 9) mustBe false
+    newBoard(4, 4) mustBe Dead
+    newBoard(7, 9) mustBe Dead
+  }
+
+  test(
+    "Any live cell with fewer than two live neighbours dies, as if caused by underpopulation."
+  ) {
+    val board = addNewCell(emptyBoard, 1, 1)
+    val nextGeneration = evolve(board, Size(3, 3))
+    nextGeneration(1, 1) mustBe Dead
   }
 }
