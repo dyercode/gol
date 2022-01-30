f28dc584792ddf2251c8f126bffc546e676dc7cd
Jon <dyercode@users.noreply.github.com>
2022-01-30 18:58:03 -0500

    still borked, but debug to get a string to test fix offline.

--- package.sh
+++ package.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
 mkdir dist
-# echo "scala_version=${{sbt about | grep 'The current project is built against Scala' | sed  's/[^0-9.]//g'}}" >> $GITHUB_ENV
 
-SCALA_VERSION=$(sbt about | grep "The current project is built against Scala" | sed  "s/[^0-9.]//g")
+STRING_OF_INTEREST="The current project is built against Scala"
+GREPPED=$(sbt about | grep $STRING_OF_INTEREST)
+echo $GREPPED
+SCALA_VERSION=$(echo $GREPPED | sed "s/[^0-9.]//g")
 cp js/target/scala-${SCALA_VERSION}/gol-opt/* ./dist/
 cp index.html dist/
