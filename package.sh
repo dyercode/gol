1c618aaeab445931d5d1ab64ff34b37e63935568
Jon <dyercode@users.noreply.github.com>
2022-01-30 19:21:52 -0500

    remove echo and inline variable.

--- package.sh
+++ package.sh
@@ -3,8 +3,6 @@ set -e
 mkdir dist
 
 
-GREPPED=$(sbt about | grep "The current project is built against Scala")
-echo $GREPPED
-SCALA_VERSION=$(echo $GREPPED | sed -r "s/.*The current project is built against Scala ([0-9.]+)$/\1/")
+SCALA_VERSION=$(sbt about | grep "The current project is built against Scala"| sed -r "s/.*The current project is built against Scala ([0-9.]+)$/\1/")
 cp js/target/scala-${SCALA_VERSION}/gol-opt/* ./dist/
 cp index.html dist/
