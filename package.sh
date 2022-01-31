f3f5876a1f731963b2ae05dfdb18046d3b0338bb
Jon <dyercode@users.noreply.github.com>
2022-01-30 19:15:23 -0500

    this eliminates opening zeros. may remain the trailing one.

--- package.sh
+++ package.sh
@@ -5,6 +5,6 @@ mkdir dist
 
 GREPPED=$(sbt about | grep "The current project is built against Scala")
 echo $GREPPED
-SCALA_VERSION=$(echo $GREPPED | sed "s/[^0-9.]//g")
+SCALA_VERSION=$(echo $GREPPED | sed -r "s/.*The current project is built against Scala ([0-9.]+)$/\1/")
 cp js/target/scala-${SCALA_VERSION}/gol-opt/* ./dist/
 cp index.html dist/
