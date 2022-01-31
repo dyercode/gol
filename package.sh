db7aa9438c001c81a4469e4a2b9cee05b2fc221e
Jon <dyercode@users.noreply.github.com>
2022-01-30 19:32:13 -0500

    doubt this'll do anything. honestly what is going on that .* isn't skipping junk.

--- package.sh
+++ package.sh
@@ -3,6 +3,6 @@ set -e
 mkdir dist
 
 
-SCALA_VERSION=$(sbt about | grep "The current project is built against Scala"| sed -r "s/.*The current project is built against Scala ([0-9.]+)$/\1/")
+SCALA_VERSION=$(sbt -Dsbt.log.noformat=true about | grep "The current project is built against Scala" | sed -r "s/.*The current project is built against Scala ([0-9.]+)$/\1/")
 cp js/target/scala-${SCALA_VERSION}/gol-opt/* ./dist/
 cp index.html dist/
